7902c9f591712dc96227531f4dee0279e67e56c3
Scott Peters <scott@dpeters.net>
2020-02-17 17:57:56 -0500

    Updated Field class to be a container, rather than a ndarray subclass

--- pyphasefield/pyphasefield/Engines/Diffusion.py
+++ pyphasefield/pyphasefield/Engines/Diffusion.py
@@ -5,7 +5,7 @@ def Diffusion(sim):
     dt = sim._time_step_in_seconds
     c = sim.fields[0]
     dc = dt*(sim.D*c.laplacian())
-    sim.fields[0] += dc
+    sim.fields[0].data += dc
     
 def init_Diffusion(sim, dim):
     sim.D = 0.1
@@ -14,6 +14,6 @@ def init_Diffusion(sim, dim):
     c = np.zeros(dim)
     length = dim[0]
     c[length//4:3*length//4] = 1
-    c_field = Field(c, name="c", simulation=sim)
+    c_field = Field(data=c, name="c", simulation=sim)
     sim.add_field(c_field)
     sim.set_engine(Diffusion)
--- pyphasefield/pyphasefield/field.py
+++ pyphasefield/pyphasefield/field.py
@@ -1,78 +1,68 @@
 import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 
-defaultsim = ""
-
-class Field(np.ndarray):
+class Field():
     
     #Modified subclass of ndarray, mostly following the guide from:
     #https://docs.scipy.org/doc/numpy/user/basics.subclassing.html#slightly-more-realistic-example-attribute-added-to-existing-array
     
-    def __new__(cls, input_array, name="phi", field_type="scalar", simulation=None):
-        from .simulation import Simulation
-        if simulation is None:
-            defaultsim = Simulation("")
-            simulation = defaultsim
-        # Input array is an already formed ndarray instance
-        # We first cast to be our class type
-        obj = np.asarray(input_array).view(cls)
-        # add the new attribute to the created instance
-        obj._name = name
-        obj._field_type = field_type
-        obj._simulation = simulation
-        
-        # Finally, we must return the newly created object:
-        return obj
-
-    def __array_finalize__(self, obj):
-        # see InfoArray.__array_finalize__ for comments
-        if obj is None: return
-        self._name = getattr(obj, '_name', "phi")
-        self._field_type = getattr(obj, '_field_type', "scalar")
-        self._simulation = getattr(obj, '_simulation', defaultsim)
+    def __init__(self, data=None, simulation=None, name=None, colormap=None):
+        self.data = data
+        self.name = name
+        self._simulation = simulation
+        if(colormap is None):
+            colors = [(0, 0, 1), (1, 1, 1), (1, 0, 0)]
+            cm = LinearSegmentedColormap.from_list('rgb', colors)
+            self.colormap = cm
+        else:
+            self.colormap = colormap
+            
+    def __str__(self):
+        return self.data.__str__()
         
     def gradient_cell(self):
-        number_of_dimensions = len(self.shape)
+        number_of_dimensions = len(self.data.shape)
         inverse_cell_spacing = 1./(self._simulation.get_cell_spacing())
         inverse_2dx = 0.5*inverse_cell_spacing
-        array = (np.roll(self, -1, 0) - np.roll(self, 1, 0))*inverse_2dx
+        array = (np.roll(self.data, -1, 0) - np.roll(self.data, 1, 0))*inverse_2dx
         array = array[np.newaxis] #add dimension for different partial derivatives
         for i in range(1, number_of_dimensions):
-            temp = (np.roll(self, -1, i) - np.roll(self, 1, i))*inverse_2dx
+            temp = (np.roll(self.data, -1, i) - np.roll(self.data, 1, i))*inverse_2dx
             temp = temp[np.newaxis]
             array = np.concatenate((array, temp), axis=0)
         return array
         
     def gradient_face_left(self):
-        number_of_dimensions = len(self.shape)
+        number_of_dimensions = len(self.data.shape)
         inverse_cell_spacing = 1./(self._simulation.get_cell_spacing())
-        array = (self - np.roll(self, 1, 0))*inverse_cell_spacing
+        array = (self.data - np.roll(self.data, 1, 0))*inverse_cell_spacing
         array = array[np.newaxis] #add dimension for different partial derivatives
         for i in range(1, number_of_dimensions):
-            temp = (self - np.roll(self, 1, i))*inverse_cell_spacing
+            temp = (self.data - np.roll(self.data, 1, i))*inverse_cell_spacing
             temp = temp[np.newaxis]
             array = np.concatenate((array, temp), axis=0)
         return array
     
     def gradient_face_right(self):
-        number_of_dimensions = len(self.shape)
+        number_of_dimensions = len(self.data.shape)
         inverse_cell_spacing = 1./(self._simulation.get_cell_spacing())
-        array = (np.roll(self, -1, 0) - self)*inverse_cell_spacing
+        array = (np.roll(self.data, -1, 0) - self.data)*inverse_cell_spacing
         array = array[np.newaxis] #add dimension for different partial derivatives
         for i in range(1, number_of_dimensions):
-            temp = (np.roll(self, -1, i) - self)*inverse_cell_spacing
+            temp = (np.roll(self.data, -1, i) - self.data)*inverse_cell_spacing
             temp = temp[np.newaxis]
             array = np.concatenate((array, temp), axis=0)
         return array
         
     def laplacian(self):
-        number_of_dimensions = len(self.shape)
+        number_of_dimensions = len(self.data.shape)
         inverse_cell_spacing = 1./(self._simulation.get_cell_spacing())
         inverse_dx_squared = inverse_cell_spacing**2
-        result = (np.roll(self, -1, 0) + np.roll(self, 1, 0) - 2*self)*inverse_dx_squared
+        result = (np.roll(self.data, -1, 0) + np.roll(self.data, 1, 0) - 2*self.data)*inverse_dx_squared
         for i in range(1, number_of_dimensions):
-            result += (np.roll(self, -1, i) + np.roll(self, 1, i) - 2*self)*inverse_dx_squared
+            result += (np.roll(self.data, -1, i) + np.roll(self.data, 1, i) - 2*self.data)*inverse_dx_squared
         return result
     
     def get_nonboundary_cells(self, boundary_conditions_type):
         #for periodic, this is simply the following
-        return self
+        return self.data
