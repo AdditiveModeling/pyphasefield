deb38fc65d9dc02f9dccef47dcf315e1d8decedd
Scott Peters <scott@dpeters.net>
2020-02-18 11:05:48 -0500

    removed line from warren example file which was added accidentally

--- pyphasefield/pyphasefield/examples/warren1995_example.py
+++ pyphasefield/pyphasefield/examples/warren1995_example.py
@@ -3,7 +3,6 @@ sys.path.insert(0,"../..")
 import pyphasefield as ppf
 
 sim = ppf.Simulation(save_path="data/warren1995_test")
-sim.load_simulation(5000)
 sim.init_sim_Warren1995([20, 20], diamond_size=10)
 print(sim.fields[0])
 print(sim.fields[1])
