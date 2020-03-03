4de771df88ba643ddc4a8ccd8223a2e1a58e5664
Scott Peters <scott@dpeters.net>
2020-03-03 11:25:36 -0500

    Changes 3/3/20

--- pyphasefield/setup.py
+++ pyphasefield/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyphasefield",
-    version="0.0.9",
+    version="0.0.10",
     author="Scott Peters",
     author_email="scott@dpeters.net",
     description="Python phase field simulation",
